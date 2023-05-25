42e9ecbbc3cc0d038a56cb808b2d081ff8780199
Mark Fynes <tfynes@vmware.com>
2023-05-25 14:57:01 -0400

    .

--- clusters/akslab/cluster-config/sensitive-file-structures/encrypt_and_copy_12.sh
+++ clusters/akslab/cluster-config/sensitive-file-structures/encrypt_and_copy_12.sh
@@ -1,9 +1,10 @@
-# sops encryptor 'age' key in local directory as 'key.txt'
-
-
 export SOPS_AGE_RECIPIENTS=$(cat key.txt | grep "# public key: " | sed 's/# public key: //')
 sops --encrypt tap-sensitive-values-12.yaml > tap-sensitive-values-12.sops.yaml
-cp ./tap-sensitive-values-12.sops.yaml /<path-to-local-repo-tap-gitops-sc>/clusters/akslab2/cluster-config/values/
+cp ./tap-sensitive-values-12.sops.yaml /Users/thomasfynes/PIVOTAL/TAP/tap-powertools/tap-installer-scripts/tap-gitops-sc/clusters/akslab/cluster-config/values/
 
 sops --encrypt ./user-registry-dockerconfig.yaml > user-registry-dockerconfig.sops.yaml
-cp ./user-registry-dockerconfig.sops.yaml /<path-to-local-repo-tap-gitops-sc>/clusters/akslab2/cluster-config/config/tap-install/
+cp ./user-registry-dockerconfig.sops.yaml /Users/thomasfynes/PIVOTAL/TAP/tap-powertools/tap-installer-scripts/tap-gitops-sc/clusters/akslab/cluster-config/config/tap-install/
+
+
+sops --encrypt ./sso-credentials-secret.yaml > sso-credentials-secret.sops.yaml
+cp ./sso-credentials-secret.sops.yaml /Users/thomasfynes/PIVOTAL/TAP/tap-powertools/tap-installer-scripts/tap-gitops-sc/clusters/akslab/cluster-config/config/tap-install/
